c6d13d748646981d619289bbb9db7dcdaeb3f3c7
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-02-06 19:03:35 -0800

    Initial commit with forward declaration for functions

---
+++ CS247/Labs/Lab4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef unsigned float_bits;
+
+int floatClass(float_bits f);
+float_bits float_negate(float_bits);
+float_bits float_absval(float_bits);
+float_bits float_twice(float_bits);
+float_bits float_half(float_bits);
+
+
+
+int main()
+{
+   return 0;
+}
