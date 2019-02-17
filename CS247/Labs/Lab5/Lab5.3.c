643cfb69d47edfc12cda243108a21370d15e21c5
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-02-17 14:47:32 -0800

    Updating repo

--- CS247/Labs/Lab5/Lab5.3.c
+++ CS247/Labs/Lab5/Lab5.3.c
@@ -3,3 +3,23 @@
   Single array
   Array manipulation
 */
+#include <stdio.h>
+
+int main()
+{
+   int arr[14];
+   arr[4] = 0x17;
+   arr[5] = 0x13;
+   arr[6] = 0xc;
+   arr[8] = 0x2;
+   arr[9] = 0x3;
+   arr[10] = 0x1;
+   arr[3] =  0x0;
+
+   arr[3] = (arr[4] * arr[8]);
+   arr[3] = (arr[5] * arr[9]);
+   arr[3] = (arr[6] * arr[10]);
+
+
+   return 0;
+}
