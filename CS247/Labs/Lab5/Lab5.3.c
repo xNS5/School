9ee580e0ea1597dc756a193ef673827182954e9a
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-02-17 21:21:29 -0800

    Completed all of lab 5. What a pain

--- CS247/Labs/Lab5/Lab5.3.c
+++ CS247/Labs/Lab5/Lab5.3.c
@@ -1,25 +1,21 @@
-/*
-  Note to self: compile with -fstack-protector-all
-  Single array
-  Array manipulation
-*/
+
 #include <stdio.h>
 
 int main()
 {
-   int arr[14];
+   int arr[13];
    arr[4] = 0x17;
    arr[5] = 0x13;
    arr[6] = 0xc;
    arr[8] = 0x2;
    arr[9] = 0x3;
    arr[10] = 0x1;
-   arr[3] =  0x0;
+   arr[3] = 0x0;
 
-   arr[3] += (arr[4] * arr[8]);
-   arr[3] = (arr[5] * arr[9]);
-   arr[3] = (arr[6] * arr[10]);
+  arr[3] += (arr[4] * arr[8]);
+  arr[3] += (arr[5] * arr[9]);
+  arr[3] += (arr[6] * arr[10]);
 
+  return arr[3];
 
-   return 0;
 }
