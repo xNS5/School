dcece5497af00cc6f86679106d94040bcc92124f
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-02-18 18:27:26 -0800

    Resolving issues

--- CS247/Labs/Lab5/Lab5.3.c
+++ CS247/Labs/Lab5/Lab5.3.c
@@ -7,19 +7,19 @@
 
 int main()
 {
-   int arr[10];
+   int arr[14];
+   arr[4] = 0x17;
+   arr[5] = 0x13;
+   arr[6] = 0xc;
+   arr[8] = 0x2;
+   arr[9] = 0x3;
+   arr[10] = 0x1;
+   arr[3] =  0x0;
 
-   arr[3] = 0x17;
-   arr[4] = 0x13;
-   arr[5] = 0xc;
-   arr[7] = 0x2;
-   arr[8] = 0x3;
-   arr[9] = 0x1;
-   arr[2] =  0x0;
+   arr[3] = (arr[4] * arr[8]);
+   arr[3] = (arr[5] * arr[9]);
+   arr[3] = (arr[6] * arr[10]);
 
-   arr[2] += (arr[3] * arr[7]);
-   arr[3] += (arr[5] * arr[9]);
-   arr[3] += (arr[6] * arr[10]);
 
    return 0;
 }
