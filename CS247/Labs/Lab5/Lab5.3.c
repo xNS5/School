f447b689d2579cd8e2827d355119126dd749c6e5
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-02-18 16:37:20 -0800

    Resolving issues before final commit

--- CS247/Labs/Lab5/Lab5.3.c
+++ CS247/Labs/Lab5/Lab5.3.c
@@ -1,20 +1,25 @@
+/*
+  Note to self: compile with -fstack-protector-all
+  Single array
+  Array manipulation
+*/
 #include <stdio.h>
 
 int main()
 {
-   int arr[13];
-   arr[4] = 0x17;
-   arr[5] = 0x13;
-   arr[6] = 0xc;
-   arr[8] = 0x2;
-   arr[9] = 0x3;
-   arr[10] = 0x1;
-   arr[3] = 0x0;
+   int arr[10];
 
-  arr[3] += (arr[4] * arr[8]);
-  arr[3] += (arr[5] * arr[9]);
-  arr[3] += (arr[6] * arr[10]);
+   arr[3] = 0x17;
+   arr[4] = 0x13;
+   arr[5] = 0xc;
+   arr[7] = 0x2;
+   arr[8] = 0x3;
+   arr[9] = 0x1;
+   arr[2] =  0x0;
 
-  return 0;
-
+   arr[2] += (arr[3] * arr[7]);
+   arr[3] += (arr[5] * arr[9]);
+   arr[3] += (arr[6] * arr[10]);
+   
+   return 0;
 }
