bd80c0df741d0dddffa9e35a0ccbe57414970666
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-02-18 19:52:52 -0800

    Adding comments

--- CS247/Labs/Lab5/Lab5.3.c
+++ CS247/Labs/Lab5/Lab5.3.c
@@ -1,13 +1,21 @@
 /*
-  Note to self: compile with -fstack-protector-all
-  Single array
-  Array manipulation
+  A couple of things I noticed:
+  1. I noticed that depending on the machine, the array size of this program varies.
+  On my mac computer I got array of size 10. When I run it on linux, the array size is anywhere between 11-14. The assembly instruction says
+  sub $0x40, %rsp. When I adjust the array size to anywhere between 11-14, the $0x40 doesn't change. If I changed the array size to 10 or 15, the
+  value changes to something other than $0x40. This also happens when I run it on a school linux machine. Just don't want it to affect
+  my grade.
+
+  2. I wasn't able to find a way to match what the Example 3 file had in its assembly code when I get to
+  the 'add' function. My program does essentially what the Example1.dms file does, but the assembly is slightly different.
 */
+
+
 #include <stdio.h>
 
 int main()
 {
-   int arr[14];
+   int arr[11];
    arr[4] = 0x17;
    arr[5] = 0x13;
    arr[6] = 0xc;
@@ -19,7 +27,6 @@ int main()
    arr[3] += (arr[4] * arr[8]);
    arr[3] += (arr[5] * arr[9]);
    arr[3] += (arr[6] * arr[10]);
-
 
    return 0;
 }
