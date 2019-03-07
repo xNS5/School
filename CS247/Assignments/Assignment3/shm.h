d2a1d4ba48a959b856008f699bc3fef695960d30
xNS5 <michaelkennedyns5@gmail.com>
2019-03-07 15:47:40 -0800

    Added ifndef and endf or whatever it's called

--- CS247/Assignments/Assignment3/shm.h
+++ CS247/Assignments/Assignment3/shm.h
@@ -1,10 +1,14 @@
 // <Define an enum called StatusEnus with the enumerations "INVALID", "VALID" and "CONSUMED">
 //
 // <Define a typedef structure with the enum above and an "int" variable called "data">
-
+#ifndef shm
+#define shm
 enum StatusEnus {INVALID, VALID, CONSUMED};
 
-typedef struct {
+typedef struct Data{
     enum StatusEnus status;
     int data;
+    int test;
   } Data;
+
+#endif
