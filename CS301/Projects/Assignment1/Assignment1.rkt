b0e94326aaee5a7ea6b46c6eeb8f982a40ebc0fc
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-05-08 03:32:46 -0700

    Completed 90% of the assignment, will add finishing touches this weekend

--- CS301/Projects/Assignment1/Assignment1.rkt
+++ CS301/Projects/Assignment1/Assignment1.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+; Simple function that just returns 4x+1
 ; Stage 1 
 (define alg
   (lambda (x)
@@ -8,26 +9,36 @@
 
 ; Stage 2
 (define (caller x y)
-    (define (iter x n)
-      (define prime (alg n))
-        (cond
+   (define (iter x n)
+     (define prime (alg n))
+       (if (send-help prime)
+          (iter x (+ n 1))
+          (cond
             [(< prime x) (iter x (+ n 1))]
             [(< prime y)
-             (printf "~a\n" prime)
-             (iter x (+ n 1))]))
+             (printf "~a" prime)
+             (let ([x (root prime)])
+               (displayln (format " ~a ~a" (car(cdr x)) (car x))))
+             (iter x (+ n 1))])))
              
   (iter x 0))
 
-; Stage 3
-(define (root y)
-  (define (iter z)
-    (cond
-      [(integer? (sqrt z))
-       (sqrt (- y z))
-       (sqrt z)]
-      [(not(integer? (sqrt z))) (iter ( - z 1))]))
-  (iter y))
+; Stage 2 helper
+(define (send-help x)
+  (when (integer? (sqrt x))
+    true)
+     (for ([t (in-list '(2 3 5 7))])
+       (cond
+         [(eqv? 0 (modulo x t)) true]
+         [(eqv? x t) (continue)]))
+  false)
 
-(caller 2 20)
-       
-    
+
+; Stage 3
+(define root
+  (lambda (x)
+    (let* ([y (exact-floor (sqrt x))]
+           [z (exact-floor (sqrt (- x (* y y))))])
+     (list y z))))
+
+(display (send-help 16))
