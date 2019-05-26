01213842f63f99dc908aeceb76f6f4aca4192365
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-05-25 18:01:31 -0700

    Initial commit of Assignment2

--- CS301/Projects/Assignment2/Parser.rkt
+++ CS301/Projects/Assignment2/Parser.rkt
@@ -3,3 +3,11 @@
 (define push
   (lambda (x)
     (cons x stack)))
+
+
+(define main
+  (lambda (x)
+    (let ((temp1 (push x)))
+      (let ((temp2 (push x)))
+        (display temp1)
+        (display temp2)))))
