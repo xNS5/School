8e0138e11691f2c3aeb638bdc7ff18880001b43e
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-05-27 15:42:34 -0700

    Updated parser with file ports

--- CS301/Projects/Assignment2/Parser.rkt
+++ CS301/Projects/Assignment2/Parser.rkt
@@ -1,4 +1,10 @@
-(define stack '())
+
+
+
+;Opening 3 file ports
+(define my-file (open-output-file #:mode 'text #:exists 'append "parsestack"))
+(define my-file (open-output-file #:mode 'text #:exists 'append "inputstream"))
+(define my-file (open-output-file #:mode 'text #:exists 'append "comment"))
 
 (define push
   (lambda (x)
@@ -7,6 +13,7 @@
 
 (define main
   (lambda (x)
+    
     (let ((temp1 (push x)))
       (let ((temp2 (push x)))
         (display temp1)
