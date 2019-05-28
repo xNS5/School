12fe6820019244e37ae1bc9a4042416606ffbb04
xNS5 <michaelkennedyns5@gmail.com>
2019-05-28 11:58:11 -0700

    Completed Push and Pop, reading from text file in progress

--- CS301/Projects/Assignment2/Parser.rkt
+++ CS301/Projects/Assignment2/Parser.rkt
@@ -1,15 +1,46 @@
+
 ;Opening 3 file ports
 (define my-file (open-output-file #:mode 'text #:exists 'append "parsestack"))
-(define my-file (open-output-file #:mode 'text #:exists 'append "inputstream"))
-(define my-file (open-output-file #:mode 'text #:exists 'append "comment"))
+;(define my-file (open-output-file #:mode 'text #:exists 'append "inputstream"))
+;(define my-file (open-output-file #:mode 'text #:exists 'append "comment"))
 
 ;This function reads in data from a text file.
 ;Read data
-(define reader
-  (let ((n (open-input-file "input")))
-  (let z ((x (read n)))
-    (if (eof-object? x)
-        (close-input-port n)
-        (cons x (z (read n)))))))
 
+
+(call-with-input-file "a.txt"
+  (lambda (input-port)
+    (let loop ((x (read-char input-port)))
+      (if (not (eof-object? x))
+          (begin
+            (display x)
+            (loop (read-char input-port)))))))
+
+
+(define reader
+  (let ((n (open-input-file #:mode 'text #:for-module? #t "input")))
+  (let z ((x (read n)))
+      (cond
+        ((eof-object? x) (close-input-port n) '())
+        (else (cons x (z (read n))))))))
+
+;Push and Pop
+;All I actually do for Pop is the car of a list, so why do I need a function that does that? We'll see. 
+;Syntax: src, dest
+(define pop
+  (lambda (stack)
+    (if (null? stack)
+        '()
+        (car stack))))
+
+(define push
+  (lambda (stack val)
+    (if (or (null? stack) (null? val))
+        '()
+        (append stack val))))
+  
+;Main Function
+(define parse
+  (let ((input reader))
+    (display input)))
 
