8c6b53c265de5f687372acd9b92aee73f35b4cd1
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-06-05 13:41:42 -0700

    Issue with production 8 or 9, investigating

--- CS301/Projects/Assignment2/Parser.rkt
+++ CS301/Projects/Assignment2/Parser.rkt
@@ -3,9 +3,11 @@
 
 ;================================================================================
 ;Opening 3 file ports
-;(define parse-file (open-output-file #:mode 'text #:exists 'append "parsestack"))
+(define parse-file (open-output-file #:mode 'text #:exists 'append "parsestack"))
 ;(define inputstream-file (open-output-file #:mode 'text #:exists 'append "inputstream"))
-;(define comment-file (open-output-file #:mode 'text #:exists 'append "comment"))
+(define comment-file (open-output-file #:mode 'text #:exists 'append "comment"))
+
+(display "intial stack contents\r\n" comment-file)
 
 ;Read data
 ;This function reads in data from a text file. It reads in the data and constructs a list of characters.
@@ -70,6 +72,36 @@
                   #t
                   #f)))
       #f))
+
+;Prints the stack to the text file
+;Syntax: stack
+(define print-stack
+  (lambda (x)
+    (let ((st_head (car x)))
+      (if (not (string=? st_head "$$"))
+          (begin
+            (display (string-append st_head " ") parse-file)
+            (print-stack (cdr x)))
+          (display (string-append st_head "\r\n") parse-file)))))
+
+;Prints the predictions for each production
+;Syntax: string number
+(define (print-predict inp)
+    (display (string-append (string-append "predict " inp) "\r\n") comment-file))
+
+;Prints the current top of the input stack
+;Syntax: inp (top of the input file)
+(define (print-input inp)
+  (display (string-append inp "\r\n") inputstream-file))
+
+;Error handler
+;Syntax: parse stack and the current head token
+(define (error-handler p_stack token)
+  (print-stack p_stack)
+  (display token inputstream-file)
+  (display (string-append "syntax error: " (car p_stack)) parse-file))
+  
+
 ;================================================================================
 ;Parse Table Functions
 ;Make a first and follow function
@@ -90,82 +122,162 @@
 
 ;1
 (define program
-  (lambda (stk token) ;write production 1
+  (lambda (stk token) 
     (cond
-    ((or (id? token) (string=? token "read") (string=? token "write") (string=? token "$$")) (push "stmt_list" (cdr stk)))
-     (else "Syntax Error: program"))))
+    ((or (id? token) (string=? token "read") (string=? token "write") (string=? token "$$"))
+     (begin
+       (print-stack stk)
+       (print-predict "1") ;write production 1
+     (push "stmt_list" (cdr stk))))
+     (else (push "error" stk)))))
     
 ;2
 (define stmt_list
-  (lambda (stk token) ;write production 2
+  (lambda (stk token)
      (cond
-       ((or (id? token) (string=? token "read") (string=? token "write")) (push "stmt" stk))
-       ((string=? token "$$") (cdr stk)) ;production 3
-       (else "Syntax error: stmt_list"))))
+       ((or (id? token) (string=? token "read") (string=? token "write"))
+        (begin
+        (print-stack stk)
+        (print-predict "2") ;production 2
+        (push "stmt" stk)))
+       ((string=? token "$$")
+        (begin
+        (print-stack stk)
+        (print-predict "3") ;production 3
+        (cdr stk)))
+       (else (push "error" stk)))))
 
 
 ;4-6
 (define stmt
   (lambda (stk token)
     (cond
-      ((id? token) (push "id" (push ":=" (push "expr" (cdr stk))))) ;production 4
-      ((string=? token "read") (push "read" (push "id" (cdr stk)))) ;production 5 
-      ((string=? token "write") (push "write" (push "expr" (cdr stk)))) ;production 6
-      (else "Syntax Error: stmt"))))
+      ((id? token)
+       (begin
+         (print-stack stk)
+         (print-predict "4") ;production 4
+         (push "id" (push ":=" (push "expr" (cdr stk)))))) 
+      ((string=? token "read")
+       (begin
+         (print-stack stk)
+         (print-predict "5") ;production 5 
+         (push "read" (push "id" (cdr stk)))))
+      ((string=? token "write")
+       (begin
+         (print-stack stk)
+         (print-predict "6") ;production 6
+         (push "write" (push "expr" (cdr stk)))))
+      (else (push "error" stk)))))
+       
+      
 
 ;7
 (define expr
   (lambda (stk token)
     (cond
-      ((or (id? token) (string=? token "(") (integer? (string->number token))) (push "term" (push "term_tail" (cdr stk))))
-      (else "Syntax error: expr"))))
+      ((or (id? token) (string=? token "(") (integer? (string->number token)))
+       (begin
+         (print-stack stk)
+         (print-predict "7")
+         (push "term" (push "term_tail" (cdr stk)))))
+      (else (push "error" stk)))))
 
+;Issue in 8
 ;8 and 9
 (define term_tail
   (lambda (stk token)
     (cond
-      ((or (string=? token "+") (string=? token "-")) (push "add_op" (push "term" (cdr stk)))) ;production 8
-      ((or (string=? token "$$") (id? token) (string=? token "read") (string=? token "write") (string=? token ")")) (cdr stk)); production 9
-      (else "Syntax Error: term_tail"))))
+      ((or (string=? token "+") (string=? token "-"))
+       (begin
+         (print-stack stk)
+         (print-predict "8") ;production 8
+         (push "add_op" (push "term" stk)))))
+      ((or (string=? token "$$") (id? token) (string=? token "read") (string=? token "write") (string=? token ")"))
+       (begin
+         (print-stack stk)
+         (print-predict "9")
+         (cdr stk))); production 9
+      (else (push "error" stk)))))
 
 ;10
 (define term
   (lambda (stk token)
     (cond
-      ((or (id? token) (string=? token "(") (integer? (string->number token))) (push "factor" (push "factor_tail" (cdr stk))))
-      (else "Syntax error: term"))))
+      ((or (id? token) (string=? token "(") (integer? (string->number token)))
+       (begin
+         (print-stack stk)
+         (print-predict "10") ;production 10
+         (push "factor" (push "factor_tail" (cdr stk)))))
+      (else (push "error" stk)))))
 
 ;11 and 12
 (define factor_tail
   (lambda (stk token)
     (cond
-    ((or (id? token) (string=? token "read") (string=? token "write") (string=? token ")") (string=? token "+") (string=? token "-") (string=? token "$$")) (cdr stk));production 11
-    ((or (string=? token "*") (string=? token "/")) (push "mult_op" (push "factor" (push "factor_tail" (cdr stk)))));production 12
-    (else "Syntax Error: factor_tail"))))
+    ((or (id? token) (string=? token "read") (string=? token "write") (string=? token ")") (string=? token "+") (string=? token "-") (string=? token "$$"))
+     (begin
+       (print-stack stk)
+       (print-predict "11")
+       (cdr stk)));production 11
+    ((or (string=? token "*") (string=? token "/"))
+     (begin
+       (print-stack stk)
+       (print-predict "12") ;production 12
+       (push "mult_op" (push "factor" (push "factor_tail" (cdr stk))))))
+    (else (push "error" stk)))))
 
 ;13 14 and 15
 (define factor
   (lambda (stk token)
     (cond
-      ((string=? token "(") (push "(" (push "expr" (push ")" (cdr stk))))) ;production 13
-      ((id? token) (push "id" (cdr stk))) ;production 14
-      ((integer? (string->number token)) (push token (cdr stk)))))) ;production 15
+      ((string=? token "(")
+       (begin
+       (print-stack stk)
+       (print-predict "13") ;production 13
+       (push "(" (push "expr" (push ")" (cdr stk))))))
+      ((id? token)
+       (begin
+         (print-stack stk)
+         (print-predict "14") ;production 14
+         (push "id" (cdr stk))))
+      ((integer? (string->number token))
+       (begin
+         (print-stack stk)
+         (print-predict "15")  ;production 15
+         (push token (cdr stk))))
+      (else (push "error" stk)))))
 
 ;16 and 17
 (define add_op
   (lambda (stk token)
     (cond
-      ((string=? token "+") (push "+" (cdr stk))) ;production 16
-      ((string=? token "-") (push "-" (cdr stk))) ;production 17
-      (else "Syntax error: add_op"))))
+      ((string=? token "+")
+       (begin
+         (print-stack stk)
+         (print-predict "16") ;production 16
+         (push "+" (cdr stk)))) 
+      ((string=? token "-")
+       (begin
+         (print-stack stk)
+         (print-predict "17") ;production 17
+         (push "-" (cdr stk)))) 
+      (else (push "error" stk)))))
 
 ;18 and 19
 (define mult_op
   (lambda (stk token)
     (cond
-      ((string=? token "*") (push "*" (cdr stk))) ;production 18
-      ((string=? token "/") (push "/" (cdr stk))) ;production 19
-      (else "Syntax error: mult_op"))))
+      ((string=? token "*")
+       (begin
+         (print-stack stk)
+         (print-predict "18") ;production 18
+         (push "*" (cdr stk)))) 
+      ((string=? token "/")
+       (begin
+         (print-stack stk)
+         (print-predict "19") ;production 19
+         (push "/" (cdr stk))))
+      (else (push "error" stk)))))
 
 ;================================================================================
 ;Parse
@@ -174,16 +286,19 @@
   (lambda (p_stack input)
     (let ((stack_head (car p_stack)) (input_head (car input)))
       (cond
-        ;((eq? 0 (string-length input_head)) (parse p_stack (cdr input)))
+        ((string=? stack_head "error") (error-handler p_stack input_head))
         ((or (and (string=? stack_head "id") (id? input_head)) (string=? stack_head input_head))
-         (if (and (string=? stack_head "$$") (string=? input_head "$$"))
-             (begin
-               (display " ")
-               (display input_head))
-             (begin
-               (display " ")
-               (display input_head)
-               (parse (cdr p_stack) (cdr input)))))
+              (if (and (string=? stack_head "$$") (string=? input_head "$$"))
+                  (begin
+                    (print-stack p_stack)
+                    (display " ")
+                    (display input_head)) ;Close input ports here. 
+                   (begin
+                     (print-stack p_stack)
+                     (display input_head)
+                     (display " ")
+                     (display (string-append (string-append "match " input_head) "\r\n") comment-file)
+                     (parse (cdr p_stack) (cdr input)))))
         (else(parse (table p_stack input_head) input))))))
         
 (parse (list "program" "$$") input)
