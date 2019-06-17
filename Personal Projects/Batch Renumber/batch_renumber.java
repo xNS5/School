a7a15ff084785092206753ed3b1f3893913abf74
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-06-17 16:56:28 -0700

    Experimenting with swing

--- Personal Projects/Batch Renumber/batch_renumber.java
+++ Personal Projects/Batch Renumber/batch_renumber.java
@@ -1,61 +1,30 @@
 import java.util.*;
 import java.io.*;
+import javax.swing.*;
+
+
 
 public class batch_renumber{
-   FileFilter filter{
-      public boolean accept(File file) {
-         if (file.getName().equals(".DS_Store"){
-               file.delete();
-         }
-         else if(file.isFile()){
-            return file.isFile();
-         }
-         return file.isDirectory();
-      }
-   };
 
    public static void main(String[] args){
-      try{
-         boolean done = false;
-         while (done == false)
-         {
-            int n = 1;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Is this a subpath in Photography? [Y || N]");
-            String answer = sc.nextLine();
 
-            while(!answer.equalsIgnoreCase("Y") || !answer.equalsIgnoreCase("N"))
-            {
-               System.out.println("That isn't a valid input. Try again");
-               System.out.print("Is this a subpath in Photography? [Y || N]");
-               String answer = sc.nextLine();
-            }
+      JFrame frame = new JFrame();
+      JFileChooser jfc = new JFileChooser("/users/michaelkennedy/Pictures/Photography/");
+      jfc.setDialogTitle("Open a Folder to Convert: ");
+      int returnValue = jfc.showOpenDialog(null);
+      button = new JButton("Test");
 
-            String path = parent_child(answer);
-            File file = new File(getFile() + path);
+      if (returnValue == JFileChooser.APPROVE_OPTION) {
+         File selectedFile = jfc.getSelectedFile();
 
-         }
       }
+
+      if (returnValue == JFileChooser.CANCEL_OPTION){
+         System.exit(0);
+      }
+
+
+
+
    }
-
-  private static String parent_child(String answer){
-     Scanner sc = new Scanner(System.in);
-     StringBuilder sb = new StringBuilder();
-     String path;
-
-     if (answer.equalsIgnoreCase("Y"))
-
-
-  }
-
-  private static void listFiles(String path){
-     File f = new File("/User/michaelkennedy/Pictures/Photography/"+path);
-     File[] arrList = path.listFiles(fileFilter);
-     
-  }
-  
-   private static String getDir(){  
-      return "/Users/michaelkennedy/Pictures/Photography";
-   }
-
-
+}
