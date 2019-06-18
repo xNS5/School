19256083b67af7312d8d43ad1c0fc5efc57668c3
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-06-18 00:41:32 -0700

    Final push for tonight

--- Personal Projects/Batch Renumber/batch_renumber.java
+++ Personal Projects/Batch Renumber/batch_renumber.java
@@ -1,20 +1,41 @@
 import java.io.*;
 import javax.swing.*;
-
-
+import java.awt.*;
+import java.awt.event.*;
 
 public class batch_renumber extends JFrame{
 
    public static void main(String[] args){
       container cont = new container();
+      JTextField jf1 = new JTextField(12);
       cont.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       cont.setSize(500, 500);
 
-      JButton button1 = new JButton("Choose Folder");
-      JButton button2 = new JButtin("Convert");
+      JFileChooser fc = new JFileChooser("~/Pictures/Photography");
+      int returnValue = fc.showOpenDialog(null);
+      JButton button1 = new JButton("Search for a folder");
+      JButton button2 = new JButton("Convert");
 
-
-
+      cont.add(button1);
+      cont.add(button2);
       cont.setVisible(true);
+
+      button1.addActionListener(new ActionListener(){
+         public void actionPerformed(ActionEvent e){
+            if(returnValue == JFileChooser.APPROVE_OPTION)
+            {
+
+            }else{
+               System.exit(0);
+            }
+         }
+      });
+
+      button2.addActionListener(new ActionListener(){
+         public void actionPerformed(ActionEvent e){
+            System.out.println("Bell!");
+         }
+      });
+
    }
 }
--- Personal Projects/Batch Renumber/open.java
+++ Personal Projects/Batch Renumber/open.java
@@ -4,6 +4,13 @@ public class open extends JFrame{
     public open()
     {
         JFileChooser fc = new JFileChooser("~/Pictures/Photography");
+        int returnValue = jfc.showOpenDialog(null);
 
-    }
+      if (returnValue == JFileChooser.APPROVE_OPTION) {
+         return jfc.getSelectedFile();
+      }
+      else if (returnValue == JFileChooser.CANCEL_OPTION)
+      {
+          System.exit(0);
+      }
 }
