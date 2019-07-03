b24f2f7b3bd80cfd624e9518827d73d4392b7f56
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-07-03 16:36:09 -0700

    Updating notes: layout, and DB options

---
+++ PersonalProjects/Amazon/AmazonScraper/AmazonScraper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AmazonScraper
+//
+//  Created by Michael Kennedy on 7/3/19.
+//  Copyright © 2019 Michael Kennedy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
