c5d96b1f49db3adcb85f3253dffeb598408bfbf8
Michael Kennedy <michaelkennedyns5@gmail.com>
2019-09-06 11:19:39 -0700

    Removing env files

--- PersonalProjects/Websites/Housing/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michaelkennedy/Computer Science/Github/Firebase/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
